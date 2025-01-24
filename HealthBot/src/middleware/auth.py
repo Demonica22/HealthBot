979f2502de8fbcea9d2edb037c808b2139c67260
Demonica22 <demonica2003@mail.ru>
2025-01-25 00:09:06 +0300

    Исправлена ошибка, из-за которой регистрация не проходила

--- HealthBot/src/middleware/auth.py
+++ HealthBot/src/middleware/auth.py
@@ -13,7 +13,7 @@ class AuthMiddleware(BaseMiddleware):
             event: Message,
             data: Dict[str, Any]
     ) -> Any:
-        if await get_user_by_id(event.chat.id):
+        if await get_user_by_id(event.chat.id) or await data['state'].get_data():
             return await handler(event, data)
         else:
             await registration_start(event, state=data['state'])
